86ff48821cb3e3e3240bbe45dbb4038510db2a06
artmc <artmc@LAPTOP-KKGM0I9C>
2020-05-13 09:25:33 +0100

    Add project files.

---
+++ DataTemplate/DataTemplate1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTemplate
+{
+    class DataTemplate1
+    {
+        public string StudentName { get; set; }
+        public string StudentNumber { get; set; }
+        public double GPA { get; set; }
+        public string StudentImage { get; set; }
+
+        public string GPA_Colour
+        {
+            get
+            {
+                if (GPA >= 40)
+                    return "white";
+                else
+                    return "red";
+            }
+        }
+    }
+}
---
+++ DataTemplate/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataTemplate
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<DataTemplate1> Students = new List<DataTemplate1>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            DataTemplate1 s1 = new DataTemplate1() { StudentName = "Mary", StudentNumber = "s13234534", GPA = 77.5, StudentImage = "/images/cat.jpg" };
+            DataTemplate1 s2 = new DataTemplate1() { StudentName = "Jim", StudentNumber = "s13234500", GPA = 35, StudentImage = "/images/dog.jpg" };
+
+            Students.Add(s1);
+            Students.Add(s2);
+
+            lbxStudents.ItemsSource = Students;
+        }
+    }
+}
